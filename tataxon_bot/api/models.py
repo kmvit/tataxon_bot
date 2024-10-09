dd7478d01e295ace483e055e5438844735104cd3
alena <alena@belintsev.ru>
2024-10-09 14:38:36 +0300

    Добавлена модель Category; модель Advertisement дополнена полями category, pud_date; в settings.py изменен DATABASES

--- tataxon_bot/api/models.py
+++ tataxon_bot/api/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
 
+class Category(models.Model):
+    """
+    Модель для представления категории объявления.
+    """
+    title = models.CharField(max_length=100, verbose_name='Название категории объявления')
+
+    def __str__(self):
+        """Возвращает строковое представление объекта Category."""
+        return self.title
+
+
 class Advertisement(models.Model):
     """
     Модель для представления рекламного объявления.
 
     Эта модель используется для хранения информации о рекламных объявлениях,
-    включая заголовок, краткое описание, основное изображение и ссылку
-    на полное объявление.
+    включая заголовок, краткое описание, основное изображение, ссылку
+    на полное объявление, категорию, дату и время создания.
 
     Атрибуты:
     - title: str
@@ -17,7 +28,11 @@ class Advertisement(models.Model):
     - image: ImageField
         Основное изображение объявления, которое может быть пустым.
     - full_url: str
-    Полный URL ссылки на объявление, максимальная длина 500 символов.
+        Полный URL ссылки на объявление, максимальная длина 500 символов.
+    - category: ForeignKey
+        Категория объявления
+    - pud_date: datetime
+        Дата и время создания объявления.
     """
     title = models.CharField(max_length=255,
                              verbose_name='Заголовок объявления')
@@ -30,6 +45,8 @@ class Advertisement(models.Model):
     )
     full_url = models.URLField(max_length=500,
                                verbose_name='Ссылка на полное объявление')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория объявления')
+    pud_date = models.DateTimeField(verbose_name='Дата создания объявления')
 
     def __str__(self):
         """
